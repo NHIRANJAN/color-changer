10f5fb69c0f3a01bb1eaca55642f3f0fce90d6a3
NHIRANJAN <116147848+NHIRANJAN@users.noreply.github.com>
2023-04-03 22:59:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,43 +1,19 @@
-    let ques=document.getElementById("button")
-    ques.addEventListener('click',palindrome)
-    function palindrome(){
-    let val=document.getElementById("text").value
-    let val1=val.trim().split(" ").join("");
-    console.log(val1)
-    let length=val1.length
-    let val2=""
-    let ans=""
-    console.log(length)
-        for(let i=length-1;i>=0;i--){
-            val2=val2+val1[i];
+const val=document.getElementById("item")
+const p1=document.getElementById("p1")
+
+    const wrapper=document.getElementById("wrapper")
+    const array=[1,2,3,4,5,6,7,8,9,0,'A','B','C','D','E','F']
+    function color(){
+        let val='#';
+        for(let i=0;i<6;i++){
+            val=val+array1()
         }
-        console.log(val2)
-        for(let i=0;i<=length;i++){
-            if(val1[i]==null){
-                break
-            }
-            else if(val1[i]!=val2[i]){
-                ans="no"
-                break;
-            }
-            else{
-                ans="yes"
-                continue;
-            }
-        }
-        if(ans=="yes"){
-            console.log("yes")
-            let p=document.getElementById("ans")
-            p.innerHTML="Given words are palindrome"
-        }
-        else if(ans=="no"){
-            console.log("no")
-            let p=document.getElementById("ans")
-            p.innerHTML="Given words are not palindrome"
-        }
-        else{
-            let p=document.getElementById("ans")
-            p.innerHTML="Please Enter words"
-            
-        }
-     }
+        wrapper.style.backgroundColor=val
+        p1.innerHTML= val;
+        console.log(val)
+    }
+    function array1(){
+    let result=Math.floor(Math.random(array)*16)
+    return array[result]
+    
+}
